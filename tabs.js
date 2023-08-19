2ceae4a5d11afa251bf9c4d5203f783ad23df0ea
unknown <abdulsalamabdulhameed77@gmail.com>
2023-08-19 15:10:53 +0100

    another commit

--- tabs.js
+++ tabs.js
@@ -1,1 +1,32 @@
- 
+ const allLinks = document.querySelectorAll(".tabs a");
+ const allTabs = document.querySelectorAll(".tab-content");
+
+ allLinks.forEach((elem) => {
+    elem.addEventListener('click', function(){
+        const linkId= elem.id;
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((link) => {
+            if (link.href == hrefLinkClick){
+                link.classList.add("active");   
+            } else {
+                    link.classList.remove('active');
+                }
+        });
+        allTabs.forEach((tab) =>{
+            if (tab.id.includes(linkId)){
+                tab.classList.add("tab-content--active");
+                generateTabItems(
+                    elem,
+                    tab
+                );
+            } else {
+                tab.classList.remove('tab-content--active');
+            }
+        });
+    });
+ });
+
+ const tabRecords = [{
+    scr: ''
+ }]
